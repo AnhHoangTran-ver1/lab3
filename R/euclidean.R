1ed433191b9fec6a23383e822a446d07ab65d20e
hoanganh <hoanganh.tran1891@gmail.com>
2026-09-18 11:52:22 +0200

    updating changes

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,10 +1,18 @@
 
 #' @param a A numeric value.
+#'v@param b A numeric value.
 #'
 #' @return The greatest common divisor of a and b.
 #'
 #' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 #' @export 
-my_square <- function(x){
-  x <- x*2
+euclidean <- function(a, b){
+  while(b != 0){
+    t <- b
+    b <- a %% b
+    a <- t
+  }
+  return(a)
 }
+
+euclidean(123612, 13892347912)
