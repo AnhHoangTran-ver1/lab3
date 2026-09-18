d3abb09b9baa0e657423eb47d2589c7db99898bb
hoanganh <hoanganh.tran1891@gmail.com>
2026-09-18 11:29:18 +0200

    adding all files

---
+++ R/euclidean.R
@@ -0,0 +1,10 @@
+
+#' @param a A numeric value.
+#'
+#' @return The greatest common divisor of a and b.
+#'
+#' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#' @export 
+my_square <- function(x){
+  x <- x*2
+}
