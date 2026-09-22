74477c755a47e11b89db469d92a1717b85d4dd14
hakobyantatev18 <hakobyantatev2002@gmail.com>
2026-09-22 20:34:01 +0200

    Cleaned up the Euclidean function

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,15 +1,3 @@
-#<<<<<<< Updated upstream
-#'
-#' euclidean
-#'
-#' @param a A numeric value.
-#' @param b A numeric value.
-#' 
-#' @description The best experience with the lab 3 
-#'
-#' 
-#=======
-#>>>>>>> Stashed changes
 #'
 #' Euclidean algorithm
 #' 
@@ -21,13 +9,9 @@
 #' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 #' @export 
 euclidean <- function(a, b){
-#<<<<<<< Updated upstream
   if (!is.numeric(a) || !is.numeric(b)){
     stop("The arguments are not numeric")
   }
-#=======
-#  stopifnot(is.numeric(a) && length(a) == 1 && is.numeric(b) && length(b) == 1)
-#>>>>>>> Stashed changes
   while(b != 0){
     t <- b
     b <- a %% b
